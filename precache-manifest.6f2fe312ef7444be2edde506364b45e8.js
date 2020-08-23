8f0e7d7e9546280c1c1cd2da3bc4511704dabd39
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-08-23 22:12:20 +0530

    Updates

---
+++ precache-manifest.6f2fe312ef7444be2edde506364b45e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7689f97ebf0cb6426064014f161b4e4",
+    "url": "/react-markdown.io/index.html"
+  },
+  {
+    "revision": "d10a6f4decce9a58b016",
+    "url": "/react-markdown.io/static/css/main.8c118b17.chunk.css"
+  },
+  {
+    "revision": "71af08c147c0b495cf7d",
+    "url": "/react-markdown.io/static/js/2.3b0899b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-markdown.io/static/js/2.3b0899b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10a6f4decce9a58b016",
+    "url": "/react-markdown.io/static/js/main.9b95acfb.chunk.js"
+  },
+  {
+    "revision": "652e13aebd93c42874e5",
+    "url": "/react-markdown.io/static/js/runtime-main.19081e1e.js"
+  }
+]);
